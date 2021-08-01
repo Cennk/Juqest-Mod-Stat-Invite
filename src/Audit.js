84144f854ccf245e7d72792f69c1501e7d1bfae3
Juqêst <83884544+aslandemirasd@users.noreply.github.com>
2021-08-02 00:40:37 +0300

    Update Audit.js

--- src/Audit.js
+++ src/Audit.js
@@ -184,47 +184,6 @@ function setRoles(member, params) {
     return true;
 }
 
-async function messageAudit(message) {
-    if (!message || !message.content) throw TypeError("Message Audit: Message is required!");
 
-    const Content = message.content;
-    const Mention = message.mentions;
-    const Link = new RegExp(/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi);
-    const Invite = new RegExp(/(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i);
-    const NativeEmojis = new RegExp(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c\ude32-\ude3a]|[\ud83c\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g);
-    const CustomEmojis = new RegExp(/<(?:a)?:[a-z0-9_-]{1,256}:[0-9]{16,19}>/gi);
-    const Caps = (message.content.match(/[A-ZĞÇÖIÜ]/gm) || []).length;
-    let content = undefined;
-
-    if (Link.test(Content)) content = "Link içeren mesaj.";
-    else if (Invite.test(Content)) {
-        try {
-            const FindInvite = await Moderation.fetchInvite(Content);
-            if (FindInvite.guild.id !== message.guild.id) content = "Başka sunucunun davet bağlantısı içeren mesaj.";
-        } catch(err) {
-            content = "Mesaj kanalını kirletmek."
-        }
-    } else if (Mention.everyone) message = "Everyone içeren mesaj.";
-    else if ((Mention.roles.size + Mention.users.size + Mention.channels.size) >= 3) content = "Fazla etiket içeren mesaj.";
-    else if (Swears.some(word => new RegExp("(\\b)+(" + word + ")+(\\b)", "gui").test(Content))) content = "Küfür içeren mesaj.";
-    else if ((Caps / Content.length) >= 0.7) content = "Fazla büyük harf içeren mesaj.";
-    else {
-        const Emojis = Content.match(NativeEmojis) || [];
-        const Emojis2 = Content.match(CustomEmojis) || [];
-        const Emoji = Emojis.concat(Emojis2);
-        if (Emoji.length > 5 && (Emoji.length / ((Content.replace(NativeEmojis, "").replace(CustomEmojis, "").trim()).length + Emoji.length) * 100) > 50) content = "Mesajda fazla emoji bulunuyor.";
-    }
-
-    if (content) {
-        Warning[message.author.id] = Warning[message.author.id] ? Warning[message.author.id] + 1 : 1;
-        setTimeout(() => {
-            Warning[message.author.id] = Warning[message.author.id] ? Warning[message.author.id] - 1 : delete Warning[message.author.id];
-        }, 1000 * 60 * 3);
-        
-        message.delete({ timeout: 200 });
-        if (Warning[message.author.id] >= 3) return message.channel.send(`${Prefix}mute ${message.author} 5m Uyarılara rağmen mesaj kanalını kirletmek.`);
-        message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag} uyarıldı!`, message.author.avatarURL({ dynamic: true })).setDescription(`**Sebep:** ${content}`));
-    }
-}
 
 Audit.login(AuditBotToken).then(() => console.log(`[AUDIT] ${Audit.user.username} is connected!`)).catch(() => console.error("[AUDIT] Bot is not connect!"));
